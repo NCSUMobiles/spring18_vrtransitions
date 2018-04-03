a2ae3fbcc8f3d09cff7b037a962a2daf2fff5a2d
Srujan Barai <srujan_barai@yahoo.com>
2018-04-03 14:44:53 -0400

    BabaYaga hut

---
+++ Assets/Scripts/Face.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 relativePos = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = rotation;
+    }
+}
---
+++ Assets/Scripts/FaceY.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceY : MonoBehaviour
+{
+
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
+        transform.LookAt(target);
+    }
+}
+
+//transform.Rotate(Vector3.right, 90f);
